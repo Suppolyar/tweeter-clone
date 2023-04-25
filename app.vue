9c3eb49ba71d02b115b9fb8f7c4c2f9d556a8473
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2023-04-25 22:25:43 +0400

    Initial commit, And navbar

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+    <main :class="{dark: darkMode}">
+        <div class="bg-white dark:bg-dim-900">
+            <div class="min-h-full">
+                <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+                    <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+                        <div class="sticky top-0">
+                            <SidebarLeft />
+                        </div>
+                    </div>
+<!--                    <div class="bg-blue-600">center</div>-->
+<!--                    <div class="bg-green-600">right</div>-->
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup>
+const darkMode = ref(false)
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,73 @@
+<template>
+    <aside class="h-screen flex flex-col">
+        <div class="p-2 m-2 hover:bg-blue-50 rounded-full w-min dark:hover:bg-white/20" :class="defaultTransition">
+            <nuxt-link to="/">
+                <div class="h-8 w-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
+
+        <div class="space-y-3 mt-2">
+            <SidebarLeftTab v-for="(item, index) in sidebarItems" :key="index" :active="item.active">
+                <template #icon>
+                    <Component :is="item.icon" />
+                </template>
+                <template #name>
+                    {{ item.title }}
+                </template>
+            </SidebarLeftTab>
+        </div>
+    </aside>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/24/solid";
+import { HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, DocumentIcon, UserIcon, EllipsisHorizontalCircleIcon } from '@heroicons/vue/24/outline'
+
+const { defaultTransition } = useTailwindConfig()
+
+const sidebarItems = [
+    {
+        title: 'Home',
+        icon: HomeIcon,
+        link: '#',
+        active: true
+    },
+    {
+        title: 'Explore',
+        icon: HashtagIcon,
+        link: '#'
+    },
+    {
+        title: 'Notification',
+        icon: BellIcon,
+        link: '#',
+    },
+    {
+        title: 'Messages',
+        icon: InboxIcon,
+        link: '#'
+    },
+    {
+        title: 'Bookmarks',
+        icon: BookmarkIcon,
+        link: '#'
+    },
+    {
+        title: 'Lists',
+        icon: DocumentIcon,
+        link: '#'
+    },
+    {
+        title: 'Profile',
+        icon: UserIcon,
+        link: '#'
+    },
+    {
+        title: 'More',
+        icon: EllipsisHorizontalCircleIcon,
+        link: '#'
+    },
+]
+</script>
